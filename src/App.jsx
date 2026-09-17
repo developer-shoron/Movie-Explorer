067ac619cc0cc9f312176f064a23ebb09841194b
developer-shoron <islamshoron639@gmail.com>
2026-09-17 23:53:19 +0600

    Fix Tailwind is here

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import Movies from "./pages/Movies.jsx";
 
 export default function App() {
   return (
-    <div className="flex min-h-screen flex-col bg-void">
+    <div className="flex min-h-screen flex-col bg-slate-950">
       <Navbar />
+
       <main className="flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
         </Routes>
       </main>
+
       <Footer />
     </div>
   );
